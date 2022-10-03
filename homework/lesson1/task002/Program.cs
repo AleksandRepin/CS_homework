d0e1a85229ba7fff82796c27a49fd3faaf51bcdd
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-03 16:34:51 +0500

    added homework and ignore

---
+++ homework/lesson1/task002/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = new Random().Next(-2147483648, 2147483647);
+Console.WriteLine(a);
+int b = new Random().Next(-2147483648, 2147483647);
+Console.WriteLine(b);
+int max = 0;
+int min = 0;
+if (a > b) { max = a;
+min = b;
+}
+else { max = b;
+min = a;
+}
+
+Console.Write("Наибольшее -> ");
+Console.WriteLine(max);
+Console.Write("Наименьшее -> ");
+Console.WriteLine(min);
