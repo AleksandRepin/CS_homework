a89ccc0efad5207f24a816556d757473c4848306
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-03 18:18:56 +0500

    added programs for task004

---
+++ homework/lesson1/task004/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = new Random().Next(-2147483648, 2147483647);
+int b = new Random().Next(-2147483648, 2147483647);
+int c = new Random().Next(-2147483648, 2147483647);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Наибольшее -> ");
+Console.WriteLine(max);
