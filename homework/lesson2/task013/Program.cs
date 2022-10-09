b2d6e5b830bc9dd9a2f63f8fac041a30a3325fba
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-09 23:14:33 +0500

    added void

---
+++ homework/lesson2/task013/Program.cs
@@ -0,0 +1,16 @@
+﻿
+static void task013()
+{
+    int value = new Random().Next(1, 2147483647);
+    Console.WriteLine(value);
+    if (value > 99)
+    {
+        int res = value / 100 % 10;
+        Console.WriteLine(res);
+    }
+    else
+    {
+        Console.Write("Третьей цифры нет");
+    }
+}
+task013();
