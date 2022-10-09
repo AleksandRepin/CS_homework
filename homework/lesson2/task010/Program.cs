bb6e574f415c706c3f792f7ba3b1a3e3bc345166
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-09 21:39:08 +0500

    delete and added

---
+++ homework/lesson2/task010/Program.cs
@@ -0,0 +1,15 @@
+﻿// ввести число abc
+// вычисление 
+// показ 
+
+//double res(int N);
+//int N = new Random().Next(100,999);
+//Console.writeLine(N);
+//res = N / 10 % 10;
+
+void task010(int N = new Random().Next(100,999));
+{
+    Console.WriteLine(N);
+    int res = N / 10 % 10;
+}
+Console.WriteLine(res);
