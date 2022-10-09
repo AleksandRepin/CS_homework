3a6014b60c1ed48fc9057d555a3c545f80628b79
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-10 00:51:24 +0500

    added task

---
+++ homework/lesson2/task015/Program.cs
@@ -0,0 +1,17 @@
+﻿static void task015()
+{
+    int[] weekDays = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+    int day = new Random().Next(1, 8);
+    Console.WriteLine(day);
+
+    if (day > 5)
+    {
+        Console.Write("Выходной");
+    }
+    else
+    {
+
+    }
+}
+
+task015();
