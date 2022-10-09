94664091040bb6f3b7cbdc7f77994d9e18cb55bd
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-10 01:23:44 +0500

    added task013

---
+++ homework/lesson3/task013/Program.cs
@@ -0,0 +1,17 @@
+﻿task013();
+
+static void task013()
+{
+    int value = new Random().Next(10000, 99999);
+    Console.WriteLine(value);
+    int res1 = value % 100;
+    int res2 = value / 1000;
+    if (res1 == res2)
+    {
+        Console.WriteLine(value + " это число палиндром");
+    }
+    else
+    {
+
+    }
+}
