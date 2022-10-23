f920f6bcb03e851a66a4220fbfc4f33b663d5234
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-23 20:59:30 +0500

    Added task025

--- homework/lesson4/task025/Program.cs
+++ homework/lesson4/task025/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿double metod(double A, int B)
+{
+    int i = 0;
+    double result = 1;
+    while (i < B)
+    {
+        result = result * A;
+        i++;
+    }
+    return result;
+
+}
+Console.Write(" Enter a number A: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write(" Enter a number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+double res = metod(A, B);
+Console.WriteLine(res);
