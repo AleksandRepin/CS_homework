b43e4d9a94231c1bab72494d91290bea6fee0b5a
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-24 01:08:59 +0500

    task027

---
+++ homework/lesson4/task027/Program.cs
@@ -0,0 +1,16 @@
+﻿
+double sum()
+{
+    int s = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        s = N[i] + N[i + 1];
+        return s;
+    }
+    Console.WriteLine(s);
+}
+sum();
+// Console.Write(" Enter a number N: ");
+// string N = Console.ReadLine();
+// sum(res);
+// Console.WriteLine(res);
