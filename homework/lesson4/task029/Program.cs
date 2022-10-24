73653bcce64b7183ac2becd2a7d4d23c238aade9
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-24 15:57:00 +0500

    Added task029

---
+++ homework/lesson4/task029/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] a)
+{
+    int L = a.Length;
+    int i = 0;
+    while (i < L)
+    {
+        a[i] = new Random().Next(-2147483648, 2147483647);
+        i++;
+    }
+}
+
+void PrintArray(int[] b)
+{
+    int k = b.Length;
+    int j = 0;
+    while (j < k)
+    {
+        Console.WriteLine(b[j]);
+        j++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
