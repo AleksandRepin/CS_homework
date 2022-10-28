f576de6e756a3151f62ce3bad53e1b4f3eb63203
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-28 20:11:43 +0500

    task008 ok+

---
+++ homework/lesson1/task008_working/Program.cs
@@ -0,0 +1,11 @@
+﻿// откуда берем значение N
+// создаем цикл с проверкой условия N % 2 == 0
+// вывод результата
+
+Console.Write("Enter a num N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+while (N > 1)
+{
+    if ( N % 2 == 0) Console.WriteLine(N);
+    N = N - 1;
+}
