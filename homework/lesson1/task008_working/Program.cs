471da2fe2b5cd09041c5339b79e1ded40829cbc9
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-10-28 20:30:18 +0500

    added task008

--- homework/lesson1/task008_working/Program.cs
+++ homework/lesson1/task008_working/Program.cs
@@ -2,10 +2,14 @@
 // создаем цикл с проверкой условия N % 2 == 0
 // вывод результата
 
-Console.Write("Enter a num N: ");
+Console.Write("Enter a num N > 1: ");
 int N = Convert.ToInt32(Console.ReadLine());
 while (N > 1)
 {
     if ( N % 2 == 0) Console.WriteLine(N);
     N = N - 1;
+}
+if (N < 1) 
+{
+    Console.Write("Enter N > 1 !!!!!");
 }
