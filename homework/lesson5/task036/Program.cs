66cd0716018ffb35a25c95dfd4055b88b624b12a
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-11-03 10:29:08 +0500

    added task036

--- homework/lesson5/task036/Program.cs
+++ homework/lesson5/task036/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных
+//  позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Random.Shared.Next(-100, 100);
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+Console.WriteLine(String.Join(' ', array));
+
+
