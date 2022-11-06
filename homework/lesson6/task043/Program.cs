c1bbc1faa0ec74a2fec32ce9984bf54f950b388e
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-11-07 00:33:26 +0500

    add task043 true

---
+++ homework/lesson6/task043/Program.cs
@@ -0,0 +1,18 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter b1 = ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter k1 = ");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter b2 = ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter k2 = ");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+
+        double x = (b2 - b1) / (k1 - k2);
+        Console.WriteLine($"({x}; {x})");
+    }
+}
