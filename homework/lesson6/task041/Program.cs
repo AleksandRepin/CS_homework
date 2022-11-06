63cbcf8aae083c43f8509000d4511ac62ea66ce5
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-11-06 23:59:37 +0500

    added task041 true

---
+++ homework/lesson6/task041/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+Console.Write("Enter M (number of numbers) = ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+
+void getSumPositiveNumbers(int[] arg)
+{
+    int sum = 0;
+    for (int i = 0; i < arg.Length; i++)
+    {
+        Console.Write("Enter number = ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n > 0)
+        {
+        sum++;
+        }
+    }
+    Console.WriteLine($"Sum Positive numbers = {sum}");
+}
+
+getSumPositiveNumbers(array);
+
+
