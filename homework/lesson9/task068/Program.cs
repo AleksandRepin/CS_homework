4373eb73d7fc9d182c91da7246af706433d81035
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-11-13 22:08:47 +0500

    added task068

---
+++ homework/lesson9/task068/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите начальное число M:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int n = int.Parse(Console.ReadLine());
+
+///Метод вычисления функции Аккермана:
+int AckermannFunction (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 || n == 0) return AckermannFunction(n - 1, 1);
+    if (m > 0 || n > 0) return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+return AckermannFunction(m, n);
+}
+
+Console.WriteLine($"A({m},{n}) = {AckermannFunction(m, n)} ");
