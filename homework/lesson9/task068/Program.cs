ec4731a71da831139efd49f03fc2fe92765ccdcd
AleksandRepin <repin.aleksandr.v@gmail.com>
2022-11-14 00:37:28 +0500

    true task 068

---
+++ homework/lesson9/task068/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите начальное число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int AckermannFunction (int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M > 0 && N == 0) return AckermannFunction(M - 1, 1);
+    if (M > 0 && N > 0) return AckermannFunction(M - 1, AckermannFunction(M, N - 1));
+return AckermannFunction(M, N);
+}
+
+Console.WriteLine($" A({M},{N}) = {AckermannFunction(M, N)}");
